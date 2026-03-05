6422dee2cd91129cf4ad05b1b82ca9173ce377c6
Pavel Novikov <dev-ape@yandex.ru>
2026-03-06 01:10:28 +0300

    updated years

--- include/minjsoncpp.h
+++ include/minjsoncpp.h
@@ -1,4 +1,4 @@
-﻿// Minimalistic JSON C++ library by Pavel Novikov (2024, 2025)
+﻿// Minimalistic JSON C++ library by Pavel Novikov (2024-2026)
 // for details see https://github.com/toughengineer/minjsoncpp
 #pragma once
 
